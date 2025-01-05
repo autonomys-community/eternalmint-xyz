112d78a8cef35f17353f5e4b885194e26f4b0412
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-01-05 10:56:14 -0500

    remove mid-header

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,22 +6,8 @@ import { LatestNFTList } from "../components/LatestNFTList";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 pb-20 sm:p-20 bg-gray-900 text-white">
+    <div className="min-h-screen p-8 pb-20 sm:p-20 text-white">
       <Header />
-      <div className="text-center mb-8">
-        <Image
-          src="/logo.svg"
-          alt="Eternal Mint Logo"
-          width={100}
-          height={100}
-          className="mx-auto mb-4"
-        />
-        <h1 className="text-4xl font-bold">Eternal Mint</h1>
-        <p className="text-lg mt-2">
-          Mint Once, Own Forever: Fully Decentralized, Eternally Accessible
-          NFTs.
-        </p>
-      </div>
       <CreateNFTForm />
       <LatestNFTList />
       <Footer />
