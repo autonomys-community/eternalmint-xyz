6361611ee08d92d2c6ea0adaf945726b6ed7fe2b
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-01-12 11:44:35 -0500

    display more nft details

--- web/src/components/NftContainer.tsx
+++ web/src/components/NftContainer.tsx
@@ -1,18 +1,27 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useEffect, useMemo, useState } from "react";
 import { Metadata, NFT } from "../types";
 
 export const NftContainer: React.FC<{ nft: NFT }> = ({ nft }) => {
   const [metadata, setMetadata] = useState<Metadata | null>(null);
 
   const handleLoadMetadata = async (cid: string) => {
-    const res = await fetch(`/api/cid/taurus/${cid}`);
-    const data = await res.json();
-    console.log("metadata", data);
-    setMetadata(data);
+    try {
+      const res = await fetch(`/api/cid/taurus/${cid}`);
+      const data = await res.json();
+      setMetadata(data);
+    } catch (error) {
+      console.error("Error loading metadata", error);
+    }
   };
+
+  const imageCid = useMemo(
+    () => (metadata && metadata.image ? metadata.image.split("/").pop() : null),
+    [metadata]
+  );
 
   useEffect(() => {
     handleLoadMetadata(nft.cid);
@@ -35,7 +44,26 @@ export const NftContainer: React.FC<{ nft: NFT }> = ({ nft }) => {
         <h3 className="text-xl font-semibold">{metadata?.name}</h3>
         <p className="text-sm text-gray-400">{metadata?.description}</p>
         <p className="text-sm text-gray-400">Quantity: {nft.quantity}</p>
-        <p className="text-sm text-gray-400">CID: {nft.cid}</p>
+        <p className="text-sm text-gray-400 break-all">
+          Metadata:{" "}
+          <Link
+            href={`${process.env.NEXT_PUBLIC_PERMANENT_STORAGE_URL}/${nft.cid}`}
+            target="_blank"
+          >
+            {nft.cid.slice(0, 6)}...{nft.cid.slice(-6)}
+          </Link>
+        </p>
+        {imageCid && (
+          <p className="text-sm text-gray-400 break-all">
+            Image:{" "}
+            <Link
+              href={`${process.env.NEXT_PUBLIC_PERMANENT_STORAGE_URL}/${imageCid}`}
+              target="_blank"
+            >
+              {imageCid.slice(0, 6)}...{imageCid.slice(-6)}
+            </Link>
+          </p>
+        )}
       </div>
     </li>
   );
